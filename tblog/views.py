b57ccb4ce3cb2f576bbb6331257abcf18bf575dd
s-hama <s-hama@sigma-se.jp>
2024-06-14 07:03:10 +0900

    feat: add BaseListView function

--- tblog/views.py
+++ tblog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.views import generic
+from .models import Post
+import logging
+ 	
+class BaseListView(generic.ListView):
+    paginate_by = 10 
+    def base_queryset(self):
+        queryset = Post.objects.filter(
+            is_publick=True).order_by('-created_at')
+        logging.getLogger('command').debug('ON View.py > BaseListView')
+        return queryset
