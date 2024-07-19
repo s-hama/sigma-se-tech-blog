1a818909bae6d8706c728827cb85b274b9704cc1
s-hama <s-hama@sigma-se.jp>
2024-07-20 00:02:44 +0900

    feat: add the IP address to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&y73d7gv&^4*p2@%3yptskb@=qve=ps!&l&r!scxkm@#egb_p6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['sigma-se.com', 'www.sigma-se.com']
+ALLOWED_HOSTS = ['sigma-se.com', 'www.sigma-se.com', '162.43.85.169']
 
 
 # Application definition
